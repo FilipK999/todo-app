31e9986825028ae67b05b4e3980bc1307a6dbf55
Filip Krayem <filipkrayem@gmail.com>
2020-09-19 22:47:46 +0200

    change color theme

--- client/src/utils/theme.js
+++ client/src/utils/theme.js
@@ -1,14 +1,15 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import red from "@material-ui/core/colors/red";
+import { lightBlue } from "@material-ui/core/colors";
 
 export const theme = createMuiTheme({
   palette: {
     type: "light",
     primary: {
-      main: "#C9082A"
+      main: lightBlue[500]
     },
     secondary: {
-      main: "#17408B"
+      main: red.A200
     },
     error: {
       main: red.A700
@@ -20,10 +21,10 @@ export const darkTheme = createMuiTheme({
   palette: {
     type: "dark",
     primary: {
-      main: "#C9082A"
+      main: lightBlue[500]
     },
     secondary: {
-      main: "#17408B"
+      main: red.A200
     },
     error: {
       main: red.A700
