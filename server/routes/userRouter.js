6731b9a554537a58ad11c49a65b2c94f641b84af
Filip Krayem <filipkrayem@gmail.com>
2020-09-20 22:14:51 +0200

    Add email validation for login form

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,6 +4,8 @@ const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
 const User = require("../models/User");
 
+const re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
 router.post("/register", async (req, res) => {
   try {
     const { email, password, password2, username } = req.body;
@@ -14,7 +16,7 @@ router.post("/register", async (req, res) => {
         message: "All fields are required"
       });
     }
-    const re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
     if (!re.test(String(email.toLowerCase()))) {
       return res.send({
         failure: true,
@@ -60,12 +62,21 @@ router.post("/register", async (req, res) => {
 router.post("/login", (req, res) => {
   try {
     const { email, password } = req.body;
+
     if (!email || !password) {
       return res.send({
         failure: true,
         message: "All fields are required"
       });
     }
+
+    if (!re.test(String(email.toLowerCase()))) {
+      return res.send({
+        failure: true,
+        message: "Provide a valid email address"
+      });
+    }
+
     User.findOne({ email: email.toLowerCase().trim() }, async (err, user) => {
       if (!user) {
         return res.send({
